7a5e50571b4c0fbff27ee15c1f3cc6504a190c4b
Kevin Velghe <kevin@paretje.be>
2017-02-08 19:49:14 +0100

    Stop using tmux by default

--- config.def.h
+++ config.def.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-glob", "-modi", "run,ssh", "-ssh-client", "rtmux", "-show", "run", NULL };
-static const char *termcmd[]  = { "urxvtcd", "-e", "tmux-attach", NULL };
+static const char *termcmd[]  = { "urxvtcd", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
